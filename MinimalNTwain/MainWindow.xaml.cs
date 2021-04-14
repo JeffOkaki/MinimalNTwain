451cc4b223697fff6374b9372fcf0b079e2bb402
jpowell <jpowell@LAPTOP-JQ67SJRR>
2021-04-14 14:46:16 -0600

    Remove unused code and rename variables

--- MinimalNTwain/MainWindow.xaml.cs
+++ MinimalNTwain/MainWindow.xaml.cs
@@ -29,79 +29,59 @@ namespace MinimalNTwain
 
         public void DoTwainStuff()
         {
-            var appId = TWIdentity.CreateFromAssembly(DataGroups.Image, Assembly.GetExecutingAssembly());
-            Session = new TwainSession(appId);
+            var applicationID = TWIdentity.CreateFromAssembly(DataGroups.Image, Assembly.GetExecutingAssembly());
+            Session = new TwainSession(applicationID);
             Session.TransferReady += Session_TransferReady;
             Session.DataTransferred += Session_DataTransferred;
             Session.Open();
-            DataSource myDS = Session.FirstOrDefault();
-            myDS.Open();
-            myDS.Enable(SourceEnableMode.NoUI, false, new WindowInteropHelper(this).Handle);
+            DataSource dataSource = Session.FirstOrDefault();
+            dataSource.Open();
+            dataSource.Enable(SourceEnableMode.NoUI, false, new WindowInteropHelper(this).Handle);
         }
 
         void Session_TransferReady(object sender, TransferReadyEventArgs e)
         {
-            var mech = Session.CurrentSource.Capabilities.ICapXferMech.GetCurrent();
-            if (mech == XferMech.File)
+            var transferMechanism = Session.CurrentSource.Capabilities.ICapXferMech.GetCurrent();
+            if (transferMechanism == XferMech.File)
             {
                 var formats = Session.CurrentSource.Capabilities.ICapImageFileFormat.GetValues();
-                var wantFormat = formats.Contains(FileFormat.Tiff) ? FileFormat.Tiff : FileFormat.Bmp;
+                var scannerImageFormat = formats.Contains(FileFormat.Tiff) ? FileFormat.Tiff : FileFormat.Bmp;
 
                 var fileSetup = new TWSetupFileXfer
                 {
-                    Format = wantFormat,
-                    FileName = GetUniqueName(System.IO.Path.GetTempPath(), "twain-test", "." + wantFormat)
+                    Format = scannerImageFormat,
+                    FileName = GetUniqueName(Path.GetTempPath(), "twain-test", "." + scannerImageFormat)
                 };
-            }
-            else if (mech == XferMech.Memory)
-            {
-                // ?
-
             }
         }
 
         void Session_DataTransferred(object sender, DataTransferredEventArgs e)
         {
-            ImageSource img = GenerateThumbnail(e);
-            if (img != null)
+            ImageSource image = GenerateThumbnail(e);
+            if (image != null)
             {
-                App.Current.Dispatcher.BeginInvoke(new Action(() =>
+                System.Windows.Application.Current.Dispatcher.BeginInvoke(new Action(() =>
                 {
-                    CapturedImages.Add(img);
+                    CapturedImages.Add(image);
                     TheImage.Source = CapturedImages.First();
                 }));
             }
         }
 
-
-
-
         string GetUniqueName(string dir, string name, string ext)
         {
-            var filePath = System.IO.Path.Combine(dir, name + ext);
+            var filePath = Path.Combine(dir, name + ext);
             int next = 1;
             while (File.Exists(filePath))
             {
-                filePath = System.IO.Path.Combine(dir, string.Format("{0} ({1}){2}", name, next++, ext));
+                filePath = Path.Combine(dir, string.Format("{0} ({1}){2}", name, next++, ext));
             }
             return filePath;
         }
 
-        void _session_DataTransferred(object sender, DataTransferredEventArgs e)
-        {
-            ImageSource img = GenerateThumbnail(e);
-            if (img != null)
-            {
-                App.Current.Dispatcher.BeginInvoke(new Action(() =>
-                {
-                    CapturedImages.Add(img);
-                }));
-            }
-        }
-
         ImageSource GenerateThumbnail(DataTransferredEventArgs e)
         {
-            BitmapSource img = null;
+            BitmapSource image = null;
 
             switch (e.TransferType)
             {
@@ -110,32 +90,22 @@ namespace MinimalNTwain
                     {
                         if (stream != null)
                         {
-                            img = stream.ConvertToWpfBitmap(300, 0);
+                            image = stream.ConvertToWpfBitmap(300, 0);
                         }
                     }
                     break;
                 case XferMech.File:
-                    img = new BitmapImage(new Uri(e.FileDataPath));
-                    if (img.CanFreeze)
+                    image = new BitmapImage(new Uri(e.FileDataPath));
+                    if (image.CanFreeze)
                     {
-                        img.Freeze();
+                        image.Freeze();
                     }
                     break;
                 case XferMech.Memory:
-                    // TODO: build current image from multiple data-xferred event
                     break;
             }
 
-            //if (img != null)
-            //{
-            //    // from http://stackoverflow.com/questions/18189501/create-thumbnail-image-directly-from-header-less-image-byte-array
-            //    var scale = MaxThumbnailSize / img.PixelWidth;
-            //    var transform = new ScaleTransform(scale, scale);
-            //    var thumbnail = new TransformedBitmap(img, transform);
-            //    img = new WriteableBitmap(new TransformedBitmap(img, transform));
-            //    img.Freeze();
-            //}
-            return img;
+            return image;
         }
 
 
